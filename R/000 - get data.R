e18bcd02c405b0bf8d00bf7e8d94b49b9200ca2c
Jindra Lacko <jindra.lacko@gmail.com>
2022-08-09 13:59:09 +0200

    načtení dat 1st service

---
+++ R/000 - get data.R
@@ -0,0 +1,66 @@
+# stahne data z https://www.mdcr.cz/ do /data
+# tečka, to je vše :)
+
+library(dplyr)
+
+mesice <- stringr::str_pad(1:12, width = 2, side = "left", pad = "0")
+
+aktualni_rok <- paste0('REG22',mesice)
+
+historie <- expand.grid(paste0('REG',19:21), mesice) %>% 
+  mutate(Var3 = paste0(Var1, Var2)) %>% 
+  pull(Var3)
+
+# stahnout historii - je úplná (seběhne vždy 4 roky × 12 měsíců = 48 souborů)
+
+for (soubor in historie) {
+  
+  # formát na zdrojí není zcela konzistentní; je třeba zkusit dvě možnosti a uvidět...
+  zipak <- paste0("https://www.mdcr.cz/getattachment/Statistiky/Silnicni-doprava/Centralni-registr-vozidel/Mesicni-statistiky-2018/", 
+                  soubor, ".zip.aspx?lang=cs-CZ")
+  excel <- paste0("https://www.mdcr.cz/getattachment/Statistiky/Silnicni-doprava/Centralni-registr-vozidel/Mesicni-statistiky-2018/", 
+                  soubor, ".xlsx.aspx?lang=cs-CZ")
+  
+  # když soubor již existuje >> přeskočit
+  if(!file.exists(paste0("./data/", soubor, ".xlsx"))) { 
+    
+    # zkusit zipák, jestli je na zdroji - pokud ano, stahnout a odzipovat
+    if(!httr::http_error(zipak)) {
+      
+      curl::curl_download(url = zipak, destfile = paste0("./data/", soubor, ".zip"))
+      unzip(zipfile = paste0("./data/", soubor, ".zip"), exdir = "./data")
+      file.remove(paste0("./data/", soubor, ".zip"))
+      
+    }
+    
+    # nezávisle na úspěchu stahování zipáku zkusit na zdroji excel
+    if(!httr::http_error(excel)) curl::curl_download(url = excel, destfile = paste0("./data/", soubor, ".xlsx"))
+  }
+  
+}
+
+# stahnout běžný rok - není garance 12 souborů ročně
+
+for (soubor in aktualni_rok) {
+  
+  zipak <- paste0("https://www.mdcr.cz/getattachment/Statistiky/Silnicni-doprava/Centralni-registr-vozidel/Mesicni-statistiky-2022,/", 
+                  soubor, ".zip.aspx?lang=cs-CZ")
+  excel <- paste0("https://www.mdcr.cz/getattachment/Statistiky/Silnicni-doprava/Centralni-registr-vozidel/Mesicni-statistiky-2022,/", 
+                  soubor, ".xlsx.aspx?lang=cs-CZ")
+
+  
+  if(!file.exists(paste0("./data/", soubor, ".xlsx"))) {
+    
+    if(!httr::http_error(zipak)) {
+      
+      curl::curl_download(url = zipak, destfile = paste0("./data/", soubor, ".zip"))
+      unzip(zipfile = paste0("./data/", soubor, ".zip"), exdir = "./data")
+      file.remove(paste0("./data/", soubor, ".zip"))
+ 
+     }
+  
+    if(!httr::http_error(excel)) curl::curl_download(url = excel, destfile = paste0("./data/", soubor, ".xlsx"))
+  }
+  
+}
+
---
+++ R/001 - digest data.R
@@ -0,0 +1,83 @@
+# projede všechny excely v adresáři data a uloží obsah do 
+# databáze typu sqlite pro budoucí zpracování
+
+library(dplyr)
+library(DBI)
+library(RSQLite)
+library(readxl)
+library(fs)
+
+sloupce <- c("pcv",
+             "kategorie",
+             "vin",
+             "cislo_tp",
+             "novost_ojetost",
+             "datum_registrace_cr",
+             "datum_registrace_kdekoliv",
+             "hmotnost",
+             "druh_provozovatele",
+             "leasing",
+             "ico_provozovatele",
+             "ico_vlastnika",
+             "cislo_ztp",
+             "okres_registrace",
+             "orp_registrace",
+             "id_barvy_hlavni",
+             "id_barvy_vedlejsi",
+             "spis_prestavby",
+             "tovarni_znacka",
+             "obchodni_oznaceni",
+             "znacka_oznaceni")
+
+ddl_registrace <- "CREATE TABLE `registrace` (
+                         `pcv` REAL,
+                         `kategorie` TEXT,
+                         `vin` TEXT,
+                         `cislo_tp` TEXT,
+                         `novost_ojetost` TEXT,
+                         `datum_registrace_cr` TEXT,
+                         `datum_registrace_kdekoliv` TEXT,
+                         `hmotnost` REAL,
+                         `druh_provozovatele` REAL,
+                         `leasing` TEXT,
+                         `ico_provozovatele` REAL,
+                         `ico_vlastnika` REAL,
+                         `cislo_ztp` TEXT,
+                         `okres_registrace` TEXT,
+                         `orp_registrace` TEXT,
+                         `id_barvy_hlavni` REAL,
+                         `id_barvy_vedlejsi` REAL,
+                         `spis_prestavby` TEXT,
+                         `tovarni_znacka` TEXT,
+                         `obchodni_oznaceni` TEXT,
+                         `znacka_oznaceni` TEXT
+                  );"
+
+excely <- fs::dir_info("./data", glob = "*.xlsx") # najít všecny excely
+
+con <- DBI::dbConnect(RSQLite::SQLite(), "./data/auta.sqlite")
+
+# zahodit co bylo...
+result <- dbSendQuery(con, "drop table registrace;")
+dbClearResult(result) 
+
+# vytvořit novou, čistou registraci
+result <- dbSendQuery(con, ddl_registrace)
+dbClearResult(result) 
+
+for (soubor in excely$path) {
+  
+  # načíst excel
+  wrk_excel <- read_excel(soubor,
+                          col_names = sloupce,
+                          range = cell_cols("A:U"),
+                          guess_max = Inf) %>% 
+    mutate(datum_registrace_cr = as.character(as.Date(as.character(datum_registrace_cr), format = "%Y%m%d"))) %>% 
+    mutate(datum_registrace_kdekoliv = as.character(as.Date(as.character(datum_registrace_kdekoliv), format = "%Y%m%d")))
+
+  # uložit do databáze
+  DBI::dbAppendTable(con, "registrace", wrk_excel)
+  
+}
+
+DBI::dbDisconnect(con) # poslední zhasne...
